78a0b8f763499f933f7a799b9c39802f5005a58b
Karan Rampal <karan.rampal@hm.com>
2022-08-29 13:35:29 +0200

    Add brackets solution

--- week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -14,18 +14,25 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next, i + 1))
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            tmp = opening_brackets_stack.pop() if opening_brackets_stack else None
+            if not tmp or not are_matching(tmp.char, next):
+                return i + 1 
 
+    idx = opening_brackets_stack.pop().position if opening_brackets_stack else 0
+    return idx
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-
+    if mismatch:
+        print(mismatch)
+    else:
+        print("Success")
 
 if __name__ == "__main__":
     main()
