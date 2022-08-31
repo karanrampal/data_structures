ab112671d0251fbc241a45831c97f238c00cb334
Karan Rampal <karan.rampal@hm.com>
2022-08-31 14:42:48 +0200

    Add max window solution

--- week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
+++ week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
@@ -1,5 +1,33 @@
 # python3
 
+class QueueWithMax:
+    def __init__(self):
+        self.__queue = []
+        self.__max = []
+
+    def Enque(self, a):
+        self.__queue.append(a)
+        while self.__max and (a > self.__max[-1]):
+            self.__max.pop(-1)
+        self.__max.append(a)
+
+    def Deque(self):
+        assert len(self.__queue)
+        val = self.__queue.pop(0)
+        if val == self.__max[0]:
+            self.__max.pop(0)
+
+    def Max(self):
+        assert len(self.__queue)
+        return max(self.__queue)
+    
+    def Max_fast(self):
+        assert len(self.__queue) 
+        return self.__max[0]
+
+    def Size(self):
+        return len(self.__queue)
+
 
 def max_sliding_window_naive(sequence, m):
     maximums = []
@@ -8,11 +36,25 @@ def max_sliding_window_naive(sequence, m):
 
     return maximums
 
+def max_sliding_window_fast(sequence, m):
+    maximums = []
+    q = QueueWithMax()
+    for i in sequence:
+        if q.Size() == m:
+            maximums.append(q.Max_fast())
+            q.Deque()
+        q.Enque(i)
+    maximums.append(q.Max_fast())
+
+    return maximums
+
+
 if __name__ == '__main__':
     n = int(input())
     input_sequence = [int(i) for i in input().split()]
     assert len(input_sequence) == n
     window_size = int(input())
 
-    print(*max_sliding_window_naive(input_sequence, window_size))
+    #print(*max_sliding_window_naive(input_sequence, window_size))
+    print(*max_sliding_window_fast(input_sequence, window_size))
 
