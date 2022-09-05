1cd1be49e33df8f00e597ead16573de610e52258
Karan Rampal <karan.rampal@hm.com>
2022-09-05 11:49:59 +0200

    Add disjoint sets docs

--- week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
+++ week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
@@ -1,4 +1,71 @@
 # python3
+from signal import set_wakeup_fd
+from typing import List
+
+class BinaryMinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def size(self) -> int:
+        return len(self.heap)
+
+    def get_parent(self, i: int) -> int:
+        return (i - 1 ) // 2 if i > 0 else 0
+
+    def left_child(self, i: int) -> int:
+        return i * 2 + 1
+
+    def right_child(self, i: int) -> int:
+        return (i + 1) * 2
+
+    def sift_up(self, i: int) -> None:
+        while (i > 0) and (self.heap[self.get_parent(i)] > self.heap[i]):
+            self.heap[i], self.heap[self.get_parent(i)] = self.heap[self.get_parent(i)], self.heap[i]
+            i = self.get_parent(i)
+
+    def sift_down(self, i: int) -> None:
+        cur = i
+        left = self.left_child(i)
+        if (left < len(self.heap)) and (self.heap[left] < self.heap[cur]):
+            cur = left
+        right = self.right_child(i)
+        if (right < len(self.heap)) and (self.heap[right] < self.heap[cur]):
+            cur = right
+        if cur != i:
+            self.heap[cur], self.heap[i] = self.heap[i], self.heap[cur]
+            self.sift_down(cur)
+
+    def insert(self, x: int) -> None:
+        self.heap.append(x)
+        self.sift_up(self.size() - 1)
+
+    def get_min(self) -> int:
+        return self.heap[0]
+
+    def extract_min(self) -> int:
+        res = self.heap[0]
+        self.heap[0] = self.heap[self.size() - 1]
+        self.heap.pop()
+        self.sift_down(0)
+        return res
+
+    def remove(self, i: int) -> None:
+        self.heap[i] = self.get_min() - 1
+        self.sift_up(i)
+        self.extract_min()
+
+    def change_priority(self, i: int, p: int) -> None:
+        old_p = self.heap[i]
+        self.heap[i] = p
+        if p > old_p:
+            self.sift_up(i)
+        else:
+            self.sift_down(i)
+
+    def build_heap(self, arr: List[int]) -> None:
+        self.heap = arr
+        for i in range(self.size() // 2, -1, -1):
+            self.sift_down(i)
 
 
 def build_heap(data):
